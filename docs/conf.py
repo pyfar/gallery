a1b05d9c73609b1b2dac513a0c81bfa73d90a8bc
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-02-23 15:57:30 +0100

    remove static path (currently not used) from config

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ intersphinx_mapping = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # -- Options for LaTeX output ------------------------------------------
