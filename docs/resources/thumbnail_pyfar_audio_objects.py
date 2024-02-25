1ee14cd02a7d32510dfb054c1919ee2c329ea3d9
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2024-02-25 18:14:10 +0100

    improve thumbnail

--- docs/resources/thumbnail_pyfar_audio_objects.py
+++ docs/resources/thumbnail_pyfar_audio_objects.py
@@ -9,6 +9,6 @@ signal = [[np.sin(np.linspace(0, 2*np.pi, 1024))],
           [np.sin(np.linspace(0, 10*np.pi, 1024)) * .25]]
 
 signal = pf.Signal(signal, 1024)
-ax = pf.plot.time(signal, linewidth=4)
+ax = pf.plot.time(signal, linewidth=8)
 plt.axis('off')
 plt.savefig('thumbnail_pyfar_audio_objects.png')
